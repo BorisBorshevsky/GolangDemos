6380057aa1e7ba8d0fb02d0d0218cde6b85d0c15
BorisBorshevsky <BorisBorshevsky@gmail.com>
2017-05-12 21:02:47 +0300

    coverage

--- main.go
+++ main.go
@@ -7,15 +7,6 @@ type Adder struct {
 func (a *Adder) Add(i int) {
 	if i == 7 {
 		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
-		panic("Boom")
 	}
 	a.sum += i
 }
